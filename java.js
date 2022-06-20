4cba6a006a09b31e0c967f9ecbcf04a52c888a65
Tyler Fribourg <tyler0153@gmail.com>
2022-06-20 12:03:10 -0400

    Add player choice prompt

--- java.js
+++ java.js
@@ -1,3 +1,7 @@
+ pSelect = prompt('what is your choice?');
+
+
+
 function compPlay(){
     let compChoice = ['rock', 'paper', 'scissor'];
     let choice = compChoice[Math.floor(Math.random()* compChoice.length)];
