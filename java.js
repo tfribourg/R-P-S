3fb22937f5035f183e7db9764762eb8a491747d5
Tyler Fribourg <tyler0153@gmail.com>
2022-06-20 13:09:08 -0400

    Initialize cSelect in global scope

--- java.js
+++ java.js
@@ -1,10 +1,27 @@
  pSelect = prompt('what is your choice?');
+ pSelect = pSelect.toLowerCase();
+ 
+
 
 
 
 function compPlay(){
     let compChoice = ['rock', 'paper', 'scissor'];
-    let choice = compChoice[Math.floor(Math.random()* compChoice.length)];
-    return choice
+    let cSelect = compChoice[Math.floor(Math.random()* compChoice.length)];
+    return cSelect
 }
 
+let cSelect = compPlay();
+
+
+function playRound(pSelect, cSelect) {
+    if (pSelect == 'rock' & cSelect == 'rock'){
+        return 'Tie'
+    }
+    if (pSelect == 'rock' & cSelect == 'paper'){
+        return 'Lose'
+
+    }
+
+
+}
